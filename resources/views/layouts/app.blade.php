3d1ac6b8506d231c9a9d5258141a67efd4c62147
novaldwp <nvldwiptr@gmail.com>
2021-03-10 00:23:23 +0700

    last update, because have no connection internet before

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,9 @@
             <li class="nav-item {{ request()->is('car-selling*') ? "active":"" }}">
               <a class="nav-link" href="{{ route('car-selling.index') }}">Penjualan</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('auth.logout') }}">Logout</a>
+            </li>
           </ul>
         </div>
     </nav>
